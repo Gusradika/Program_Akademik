a6c734f462b3b47a47877a517857b8d31c8465b3
Aditya Kesuma <aditezo24@gmail.com>
2022-06-24 05:41:51 +0700

    [stable_a0.2.1] merubah sedikit history.txt dan main.java

--- main.java
+++ main.java
@@ -9,12 +9,3 @@ public class main {
         checkID.landingPage();
     }
 }
-
-/*
- * TODO
- * 1. variable inputan usernameX, passowrdX.
- * 2. buat data Login dan aplikasikan ke checkID jika benar maka berhasil login.
- * gunakan array.
- * 3. buat error code jika Nama dan Password salah.
- * 4. Ganti Scanner menjadi Buffer Reader
- */
